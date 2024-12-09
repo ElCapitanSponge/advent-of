70a64b1836c32756ec693a52b80a1c65c5859ef1
Andrew Brunker <andrew@brunker.net.au>
2024-12-09 23:03:01 +1100

    feat: 2015 day 1 part 2

--- packages/2015/day1.js
+++ packages/2015/day1.js
@@ -15,4 +15,24 @@ function Day1Part1(lisp) {
 	return floor;
 }
 
-export { Day1Part1 };
+/**
+ * @param {string} lisp
+ * @returns {number}
+ */
+function Day1Part2(lisp) {
+	/** @type {number} */
+	let floor = 0;
+	for (let i = 0; i < lisp.length; i++) {
+		if (lisp[i] === "(") {
+			floor++;
+		} else {
+			floor--;
+		}
+		if (floor === -1) {
+			return i + 1;
+		}
+	}
+	return -1;
+}
+
+export { Day1Part1, Day1Part2 };

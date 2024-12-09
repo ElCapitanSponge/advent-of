eb1187cb91901d222f4eb81ae2d64e395a3e7371
Andrew Brunker <andrew@brunker.net.au>
2024-12-09 22:56:18 +1100

    feat: add type handling for jsdoc/js

--- packages/2015/day1.js
+++ packages/2015/day1.js
@@ -1,7 +1,9 @@
 /**
  * @param {string} lisp
+ * @returns {number}
  */
 function Day1Part1(lisp) {
+	/** @type {number} */
 	let floor = 0;
 	for (let i = 0; i < lisp.length; i++) {
 		if (lisp[i] === "(") {
